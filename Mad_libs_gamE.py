2f7505bfeec6ecac2ec6bcd958e92ce56de071de
JoshmKat-3 <Jkatongo26@uwcchina.org>
2026-09-10 15:20:12 +0800

    Finish

---
+++ Mad_libs_gamE.py
@@ -0,0 +1,12 @@
+
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+adjective3 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+
+print(f"Yesterday i went to a {adjective1} Restaurant.")
+print(f"In the reception i saw a {noun}")
+print(f"The {noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
