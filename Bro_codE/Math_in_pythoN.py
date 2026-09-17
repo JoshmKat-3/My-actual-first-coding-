60ae0c8af9befac908d4db0d0422dba948bc13b4
JoshmKat-3 <Jkatongo26@uwcchina.org>
2026-09-17 15:16:30 +0800

    Finish

---
+++ Bro_codE/Math_in_pythoN.py
@@ -0,0 +1,19 @@
+
+Chin = 6
+
+#Chin = Chin + 1
+#Chin += 1
+#Chin = Chin - 12
+#Chin -= 12
+#Chin = Chin * 2
+#Chin *= 2
+#Chin = Chin / 3
+#Chin /= 3
+#Chin = Chin ** 2
+#Chin **= 2
+
+#remainder = Chin % 3
+
+
+#print(Chin)
+#print(remainder)
